89b0154b961eab74b8c1daf11fd86037c8531546
nopalpite <nopalpite@gmail.com>
2023-04-10 17:34:47 +0200

    add test more places than available

--- tests/unit_tests/test_booking.py
+++ tests/unit_tests/test_booking.py
@@ -60,3 +60,22 @@ def test_book_with_more_than_twelve_points(client, mocker):
     # Assert
     assert response.status_code == 400
     assert "No more than 12 points!" in str(response.data)
+
+def test_book_with_more_than_competition_places(client, mocker):
+    # Arrange
+    clubs = [Club("club_name", "club@email.com", 20)]
+    competitions = [Competition("competition_name", "2020-03-27 10:00:00", 10)]
+    form = {
+        'competition': "competition_name",
+        'club': "club_name",
+        'places': 11
+    }
+
+    # Act
+    mocker.patch.object(server, 'clubs', clubs)
+    mocker.patch.object(server, 'competitions', competitions)
+    response = client.post('/purchasePlaces', data=form)
+
+    # Assert
+    assert response.status_code == 400
+    assert "Not enough places available!" in str(response.data)
