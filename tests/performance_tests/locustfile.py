f76225973b4a688b6d32ced752c0b57757a10e8d
nopalpite <nopalpite@gmail.com>
2023-04-17 23:14:10 +0200

    add performance tests

---
+++ tests/performance_tests/locustfile.py
@@ -0,0 +1,28 @@
+from locust import HttpUser, task, between
+
+
+class LocustTestServer(HttpUser):
+    wait_time = between(1, 5)
+
+    def on_start(self):
+        self.client.get("/")
+        self.client.post("/showSummary", data={'email': "john@simplylift.co"})
+
+    @task
+    def booking_get(self):
+        self.client.get(f"/book/Fail Classic/Simply Lift")
+
+    @task
+    def booking_places_post(self):
+        self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": 0,
+                "club": "Simply Lift",
+                "competition": "Fail Classic"
+                 }
+        )
+
+    @task
+    def logout(self):
+        self.client.get("/logout")
